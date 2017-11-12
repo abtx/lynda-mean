e9f538e45bca57bf7737ceb6dd85d6d1c57f811d
abtx <artur.abato.janas@gmail.com>
2017-11-12 16:30:05 +0000

    adds validation token

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,18 +1,36 @@
 var User = require('../models/user');
+var jwt = require('jwt-simple');
+var moment = require('moment');
 
 module.exports = {
   register: function(req, res){
     console.log(req.body);
+    // check if already exists
+    User.findOne({email: req.body.email}, function(err, existingUser) {
 
-    var user = new User(req.body);
+      if(existingUser)
+        return res.status(409).send({message: 'Email is already registered'})
 
-    user.save(function(err, response){
-      if(err) {
-        res.status(500).send({
-          message: err.message
-        })
-      }
-      res.status(200);
+        var user = new User(req.body);
+
+        user.save(function(err, result){
+          if(err) {
+            res.status(500).send({
+              message: err.message
+            })
+          }
+          res.status(200).send({token: createToken(result)});
+        });
     });
   }
+}
+
+function createToken(user) {
+  var payload = {
+    sub: user._id,
+    iat: moment().unix(), // issued at time
+    exp: moment().add(14, 'days').unix() // expiry date
+  }
+  //
+  return jwt.encode(payload, 'secret')
 }
